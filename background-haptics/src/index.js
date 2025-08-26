7771d07bdeb6c6d91742f32db603a534698d5d1f
Mikhail Samin <i@mishasamin.xyz>
2025-08-26 01:53:40 +0100

    Add Android background haptics and service config

--- background-haptics/src/index.js
+++ background-haptics/src/index.js
@@ -1,7 +1,16 @@
-import { Platform } from 'react-native';
-import BackgroundHapticsModule from './module';
+import { Platform, Vibration } from 'react-native';
+
+let BackgroundHapticsModule = null;
+if (Platform.OS === 'ios') {
+  BackgroundHapticsModule = require('./module').default;
+}
 
 export function impact(style = 'medium') {
+  if (Platform.OS === 'android') {
+    const durations = { light: 20, medium: 35, heavy: 50 };
+    Vibration.vibrate(durations[style] ?? durations.medium);
+    return Promise.resolve();
+  }
   if (Platform.OS !== 'ios') {
     return Promise.resolve();
   }
